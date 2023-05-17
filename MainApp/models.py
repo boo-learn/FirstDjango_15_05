03d1d8a7c5047679284767cc95e48bf7b7a9b17b
boo <eyurchenko@specialist.ru>
2023-05-18 00:38:53 +0700

    module-3 practice part-2 complete

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=5000, default="Стартовое описание")
 
     def __repr__(self):
         return f"Item: {self.name} | {self.brand}"
